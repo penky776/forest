4d41a203bd6b7ee50ee12a30e126dc44350fcc64
penky <79907738+penky776@users.noreply.github.com>
2023-01-26 14:20:26 +0500

    added functionality to read single sub-directories

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use eframe::egui;
+use egui::Ui;
 use std::fmt;
 use std::fs;
 use std::path::PathBuf;
@@ -51,49 +52,54 @@ impl eframe::App for MyApp {
             .exact_width(300.0)
             .show(ctx, |ui| {
                 ui.heading("entry3");
-                let entry3_files = list_all_files("entry3".to_string());
-                for file in entry3_files.unwrap().iter() {
-                    if ui.button(get_file_name(file.to_path_buf())).clicked() {
-                        Command::new("xdg-open")
-                            .arg(file)
-                            .spawn()
-                            .expect("command failed");
-                    }
-                }
+                let entry3_main = list_all_files("entry3".to_string());
+                browse_dir(entry3_main, ui, &"entry3/".to_string())
             });
 
         egui::SidePanel::left("left_panel")
             .exact_width(250.0)
             .show(ctx, |ui| {
                 ui.heading("entry1");
-                let entry1_files = list_all_files("entry1".to_string());
-                for file in entry1_files.unwrap().iter() {
-                    if ui.button(get_file_name(file.to_path_buf())).clicked() {
-                        Command::new("xdg-open")
-                            .arg(file)
-                            .spawn()
-                            .expect("command failed");
-                    }
-                }
+                let entry1_main = list_all_files("entry1".to_string());
+                browse_dir(entry1_main, ui, &"entry1/".to_string())
             });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("entry2");
-            let entry2_files = list_all_files("entry2".to_string());
-            for file in entry2_files.unwrap().iter() {
-                if ui.button(get_file_name(file.to_path_buf())).clicked() {
-                    Command::new("xdg-open")
-                        .arg(file)
-                        .spawn()
-                        .expect("command failed");
-                }
-            }
+            let entry2_main = list_all_files("entry2".to_string());
+            browse_dir(entry2_main, ui, &"entry2/".to_string())
         });
     }
 }
 
+fn browse_dir(entry: Result<Vec<PathBuf>, MyError>, ui: &mut Ui, main_dir: &String) {
+    for file in entry.unwrap().iter() {
+        if check_is_file(file.to_path_buf()) {
+            make_button(file, ui);
+        } else {
+            let test = ui.collapsing(get_file_name(file.to_path_buf()), |_ui| {});
+            if test.fully_open() {
+                browse_dir(
+                    list_all_files(main_dir.to_owned() + &get_file_name(file.to_path_buf())),
+                    ui,
+                    main_dir,
+                )
+            }
+        }
+    }
+}
+
+fn make_button(file: &PathBuf, ui: &mut Ui) {
+    if ui.button(get_file_name(file.to_path_buf())).clicked() {
+        Command::new("xdg-open")
+            .arg(file)
+            .spawn()
+            .expect("command failed");
+    }
+}
+
 fn list_all_files(entry: String) -> Result<Vec<PathBuf>, MyError> {
-    let mut files = Vec::with_capacity(10);
+    let mut files = Vec::with_capacity(50); // limit of 50 files/folders in each directory
     let entries = fs::read_dir("/home/potato/Code/forest/dir/".to_owned() + &entry);
     match entries {
         Ok(entries) => {
@@ -110,4 +116,8 @@ fn list_all_files(entry: String) -> Result<Vec<PathBuf>, MyError> {
 
 fn get_file_name(path: PathBuf) -> String {
     return path.file_name().unwrap().to_string_lossy().to_string();
+}
+
+fn check_is_file(path: PathBuf) -> bool {
+    return path.is_file();
 }
